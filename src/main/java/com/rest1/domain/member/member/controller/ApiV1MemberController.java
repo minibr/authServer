ab113fa6585570bc25b8e9ccbe9450a4d0d8aa7b
Minibr <minimin9823@gmail.com>
2025-09-18 12:12:29 +0900

    feat: Add Cookie

--- src/main/java/com/rest1/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/rest1/domain/member/member/controller/ApiV1MemberController.java
@@ -4,15 +4,13 @@ import com.rest1.domain.member.member.dto.MemberDto;
 import com.rest1.domain.member.member.entity.Member;
 import com.rest1.domain.member.member.service.MemberService;
 import com.rest1.global.exception.ServiceException;
+import com.rest1.global.rq.Rq;
 import com.rest1.global.rsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiV1MemberController {
 
     private final MemberService memberService;
+    private final Rq rq;
 
     record JoinReqBody(
             @NotBlank
@@ -87,6 +86,8 @@ public class ApiV1MemberController {
             throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
         }
 
+        rq.addCookie("apiKey", member.getApiKey());
+
         return new RsData(
                 "200-1",
                 "%s님 환영합니다.".formatted(reqBody.username),
@@ -96,4 +97,25 @@ public class ApiV1MemberController {
                 )
         );
     }
+
+
+    record MeResBody(
+            MemberDto memberDto
+    ) {
+    }
+
+    @GetMapping("/me")
+    public RsData<MemberDto> me() {
+
+        Member actor = rq.getActor();
+
+        return new RsData(
+                "200-1",
+                "OK",
+                new MeResBody(
+                        new MemberDto(actor)
+                )
+        );
+    }
+
 }
